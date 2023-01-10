a33a494b136fd201671200a79b5bab783f80dbe9
Nikita Vishwakarma <113134306+NikitaVishwakarma0322@users.noreply.github.com>
2023-01-11 01:08:20 +0530

    Create Main.java

---
+++ Pizza_Bill_Generator/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pizza basePizza = new Pizza(true);
+		basePizza.addExtraCheese();
+		basePizza.addExtraTopping();
+		basePizza.takeAway();
+		basePizza.getBill();
+		
+		
+		DeluxPizza dp = new DeluxPizza(false);
+		dp.addExtraCheese();
+		
+		dp.getBill();
+		
+		
+		
+	}
+}
