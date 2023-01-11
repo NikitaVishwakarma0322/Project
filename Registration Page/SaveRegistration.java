756d8cf9f64db939c426e339436aee0af77f51ea
Nikita Vishwakarma <113134306+NikitaVishwakarma0322@users.noreply.github.com>
2023-01-11 23:46:44 +0530

    Create SaveRegistration.java

---
+++ Registration Page/SaveRegistration.java
@@ -0,0 +1,26 @@
+package p1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/save")
+public class SaveRegistration extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public SaveRegistration() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("post");
+	}
+
+}
