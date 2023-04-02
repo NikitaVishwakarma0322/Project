07992d55f8c60109a1826dd81b3c789e929e19ca
Nikita Vishwakarma <113134306+NikitaVishwakarma0322@users.noreply.github.com>
2023-04-02 15:21:59 +0530

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,17 @@
+package SystemDesign.SnakeAndLadder;
+
+public class Dice {
+
+    private int numberOfDice;
+
+    Dice(int numberOfDice){
+        this.numberOfDice = numberOfDice;
+    }
+
+    int rollDice(){
+        return ((int)(Math.random()*(6*numberOfDice-1*numberOfDice)))+1;
+
+    }
+
+
+}
